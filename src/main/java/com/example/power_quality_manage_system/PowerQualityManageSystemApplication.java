7e738bd9c8d6f25f83878a1ff488e289f4d68d88
lihui_coder <729053616@qq.com>
2022-12-13 23:57:46 +0800

    initial

---
+++ src/main/java/com/example/power_quality_manage_system/PowerQualityManageSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.power_quality_manage_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerQualityManageSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PowerQualityManageSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/power_quality_manage_system/PowerQualityManageSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.power_quality_manage_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowerQualityManageSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
